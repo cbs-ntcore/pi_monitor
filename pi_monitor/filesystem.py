56031561551e48a2fde9830e34261b0840adf023
Brett <brettgraham@gmail.com>
2020-11-24 14:16:19 -0500

    adding mp4 conversion

--- pi_monitor/filesystem.py
+++ pi_monitor/filesystem.py
@@ -10,9 +10,24 @@ import os
 import subprocess
 
 
+cmd_template = "ffmpeg -i {src} -vcodec copy {dst}"
+
+
+def build_conversion_command(src, dst=None):
+    if not os.path.exists(src):
+        raise FileNotFoundError(f"{src} does not exist")
+    if dst is None:
+        dst = os.path.basename(src) + '.mp4'
+    if dst == src:
+        raise FileExistsError(f"{src} and {dst} cannot be equal")
+    if not os.path.dirname(dst):
+        os.makedirs(os.path.dirname(dst))
+    return cmd_template.format(src=src, dst=dst)
+
+
 class FileSystem:
     def __init__(self):
-        pass
+        self.conversion_process = None
 
     def get_disk_space(self, directory='/'):
         cmd = "df -h %s" % directory
@@ -27,9 +42,29 @@ class FileSystem:
         fns = [os.path.splitext(fn) for fn in os.listdir(directory)]
         return fns
 
+    def is_conversion_running(self):
+        if self.conversion_process is None:
+            return False
+        # check if process is done
+        r = self.conversion_process.poll()
+        if r is None:
+            return True
+        self.conversion_process = None
+        if r == 0:  # all ok
+            return False
+        raise Exception(f"Conversion failed with return code {r}")
+
     def convert_video(self, filename):
-        # TODO queue up thread to convert video
-        pass
+        # check existing conversion is done
+        if self.is_conversion_running():
+            raise Exception("Only 1 conversion allowed at a time")
+
+        # start conversion
+        self.conversion_process = subprocess.Popen(
+            build_conversion_command(filename))
+
+        # check conversion is running
+        return self.is_conversion_running()
 
     def get_file(self, filename):
         pass
