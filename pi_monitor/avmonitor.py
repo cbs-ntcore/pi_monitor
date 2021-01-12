b4384099a705024c823ff5f6c43f93865a4b92a7
Brett <brettgraham@gmail.com>
2021-01-12 11:32:55 -0500

    use alsaaudio for avmonitor instead of pyaudio

--- pi_monitor/avmonitor.py
+++ pi_monitor/avmonitor.py
@@ -2,41 +2,20 @@
 Web server running on a pi with a camera and microphone
 """
 
+import logging
 import os
 import re
 import threading
 import time
 import wave
 
-import pyaudio
+import alsaaudio
 
 from . import backend
 from . import camera
 from . import filesystem
 from . import mic
 from . import sysctl
-
-
-stream_config = {
-    'format': pyaudio.paInt16,
-    'channels': 1,
-    'rate': 256000,
-    'frames_per_buffer': 2048,
-    'input': True,
-}
-
-
-def find_audio_device_index_by_pattern(interface, pattern):
-    info = interface.get_host_api_info_by_index(0)
-    numdevices = info.get('deviceCount')
-
-    for i in range(numdevices):
-        dev_info = interface.get_device_info_by_host_api_device_index(0, i)
-        if dev_info.get('maxInputChannels') > 0:
-            name = dev_info.get('name')
-            if re.match(pattern, name):
-                return i
-    raise Exception(f"No device found with name {name}")
 
 
 class Mic(threading.Thread):
@@ -46,55 +25,59 @@ class Mic(threading.Thread):
 
         self.running = False
 
-        self.wav_file = None
-        self.stream = None
+        self._wav_file = None
+        self._pcm = None
+
+        self._device = 'dsnoop:CARD=Ultrasound,DEV=0'
+        self._rate = 256000
+        self._format = alsaaudio.PCM_FORMAT_S16_LE
+        self._channels = 1
+        self._period_size = 2048
+
+        self._format_width = 2
 
     def run(self):
         self.running = True
-        try:
-            self.interface = pyaudio.PyAudio()
-        except Exception as e:
-            logging.error(f"Failed to open audio interface: {e}")
-            self.running = False
-            return
-
-        open_stream_cfg = stream_config.copy()
-        open_stream_cfg['input_device_index'] = find_audio_device_index_by_pattern(
-            self.interface, 'Pettersson')
 
         # compute delay time to wait 1/2 buffer
-        buffer_delay_s = (
-            open_stream_cfg['frames_per_buffer'] / open_stream_cfg['rate'])
+        buffer_delay_s = self._period_size / self._rate
 
         while self.running:
             # by default, wait 1 ms
             delay_s = 0.001
             with self.lock:
-                if self.wav_file is not None:
-                    if self.stream is None:
-                        # stream has not yet been opened, open stream
-                        self.stream = self.interface.open(**open_stream_cfg)
-                    bs = self.stream.read(open_stream_cfg['frames_per_buffer'])
-                    self.wav_file.writeframes(bs)
-                    # stream.read will block up to 1 buffer so wait a bit longer
-                    # to allow function outside thread to modify state
+                if self._wav_file is not None:
+                    if self._pcm is None:
+                        # mic has not yet been opened, open mic
+                        self._pcm = alsaaudio.PCM(
+                            type=alsaaudio.PCM_CAPTURE, device=self._device)
+                        self._pcm.setrate(self._rate)
+                        self._pcm.setformat(self._format)
+                        self._pcm.setchannels(self._channels)
+                        self._pcm.setperiodsize(self._period_size)
+                    n, bs = self._pcm.read()
+                    if n > 0:
+                        self._wav_file.writeframes(bs)
+                    elif n < 0:  # dropped frames
+                        logging.error(f"Dropped audio frames {n}")
+                    # x.read will block up to 1 buffer so wait half that
+                    # to allow functions outside thread to modify state
                     delay_s = buffer_delay_s
                 else:  # wav_file is None
-                    # file is None/closed, close stream
-                    if self.stream is not None:
-                        self.stream.stop_stream()
-                        self.stream.close()
-                        self.stream = None
+                    # file is None/closed, close mic
+                    if self._pcm is not None:
+                        self._pcm.close()
+                        self._pcm = None
             time.sleep(delay_s)
 
         self.interface.terminate()
 
     def _open_wav_file(self, filename):
         """Do not call this without first acquiring the lock"""
-        self.wav_file = wave.open(filename, 'wb')
-        self.wav_file.setnchannels(stream_config['channels'])
-        self.wav_file.setframerate(stream_config['rate'])
-        self.wav_file.setsampwidth(pyaudio.get_sample_size(stream_config['format']))
+        self._wav_file = wave.open(filename, 'wb')
+        self._wav_file.setnchannels(self._channels)
+        self._wav_file.setframerate(self._rate)
+        self._wav_file.setsampwidth(self._format_width)
 
     def start_recording(self, filename):
         with self.lock:
@@ -102,13 +85,13 @@ class Mic(threading.Thread):
 
     def split_recording(self, filename):
         with self.lock:
-            self.wav_file.close()
+            self._wav_file.close()
             self._open_wav_file(filename)
 
     def stop_recording(self):
         with self.lock:
-            self.wav_file.close()
-            self.wav_file = None
+            self._wav_file.close()
+            self._wav_file = None
 
     def stop(self):
         with self.lock:
